897f6e8e863b9b2c4af36528b72402ceb4dfb0fa
Rishi Chourasia <rishichourasia2100@gmail.com>
2021-06-23 01:58:05 +0530

    "tha 10 update

---
+++ Day 10/script.js
@@ -0,0 +1,54 @@
+const cards = document.querySelectorAll(".memory-card");
+
+let hasFlippedCards = false;
+let firstCard, secondCard;
+let board = false;
+function flipCard() {
+	if (board) return;
+	if (this === firstCard) return;
+	this.classList.add("flip");
+
+	if (!hasFlippedCards) {
+		hasFlippedCards = true;
+		firstCard = this;
+		return;
+	}
+
+	secondCard = this;
+	ismatch();
+}
+
+function ismatch() {
+	let check = firstCard.dataset.value === secondCard.dataset.value;
+	check ? disableCard() : unflipCards();
+}
+function disableCard() {
+	firstCard.removeEventListner("click", flipCard);
+	secondCard.removeEventListner("click", flipCard);
+	resetBoard();
+}
+
+function unflipCards() {
+	board = true;
+	setTimeout(() => {
+		firstCard.classList.remove("flip");
+		secondCard.classList.remove("flip");
+		resetBoard();
+	}, 1500);
+}
+
+function resetBoard() {
+	[hasFlippedCards, board] = [false, false][(firstCard, secondCard)] = [
+		null,
+		null,
+	];
+}
+
+(function shuffle() {
+	cards.forEach((card) => {
+		let random = Math.floor(Math.random() * 12);
+		card.style.order = random;
+	});
+})();
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
