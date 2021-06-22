12e9307484e621fcbb302a6ca615b7f205468aed
Rishi Chourasia <rishichourasia2100@gmail.com>
2021-06-22 20:07:47 +0530

    Day 9 update

---
+++ Day 9/solution.js
@@ -0,0 +1,22 @@
+////Bind Method
+
+var car = {
+	registrationNumber: "GA12345",
+	brand: "Toyota",
+
+	displayDetails: function () {
+		var gadi =
+			" this is your car: " + this.registrationNumber + " " + this.brand;
+		return gadi;
+	},
+};
+
+var carname = function (car1, car2) {
+	console.log("Machine: " + this.gadi());
+	console.log("cartype: " + car1 + " " + car2);
+};
+
+var logPersonName = carName.bind(car);
+logPersonName("maruti", "gallardo");
+
+////
