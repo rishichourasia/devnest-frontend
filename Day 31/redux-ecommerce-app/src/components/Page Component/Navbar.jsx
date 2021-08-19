8189358158802438d06cec0938ef1d270cc5e64f
Rishi Chourasia <rishichourasia2100@gmail.com>
2021-08-19 20:33:55 +0530

    adding day 30 and 31

---
+++ Day 31/redux-ecommerce-app/src/components/Page Component/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+	return (
+		<div>
+			<nav>
+				<div className="nav-wrapper">
+					<span class="brand-logo">Redux Ecommerce</span>
+				</div>
+			</nav>
+		</div>
+	);
+}
