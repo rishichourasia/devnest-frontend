d03dbe85522fea413516201b985556ab6212a960
Rishi Chourasia <rishichourasia2100@gmail.com>
2021-08-26 23:58:14 +0530

    added Day33

---
+++ Day 33/typescript-app/src/App.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css'
+import TodoList from './TodoList'
+import Todos from './Todos'
+
+
+const App = () => {
+ 
+  
+
+  return (
+    
+    <div className="App">
+      <Todos />
+      
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ Day 33/typescript-app/src/Todos.tsx
@@ -0,0 +1,27 @@
+import { useState, useContext } from "react"
+
+import TodoList from "./TodoList"
+
+interface TodoType {
+    title: string,
+    done: boolean
+}
+
+const Todos = () => {
+    const [todos, setTodos] = useState<TodoType[]>([])
+    const [data, setData] = useState<string>("")
+   
+    return (
+        <div className={theme ? "todos dark" : "todos"}>
+            <input type="text" value={data} onChange={e => setData(e.target.value)}/>
+            <button onClick={() => {
+              setTodos([...todos, {title: data, done: false}])
+              setData("")
+            }}>Add Todo</button>
+        {todos.map((todo, index) => {
+          return <TodoList title={todo.title} done={todo.done} todos={todos} setTodos={setTodos} index={index} />
+        })}
+      </div>
+    )
+}
+export default Todos;
