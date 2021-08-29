988dcf8c60f0821004b8c622d2bbb428e1f13ab6
Rishi Chourasia <rishichourasia2100@gmail.com>
2021-08-29 23:11:37 +0530

    adding day 35 files

--- Day 34/redux-typescript/src/App.tsx
+++ Day 34/redux-typescript/src/App.tsx
@@ -1,39 +1,53 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addTodo, deleteTodo } from './actions/action';
-import './App.css';
-import  {reducerType}  from './reducers';
-
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, deleteTodo } from "./actions/action";
+import "./App.css";
+import { reducerType } from "./reducers";
 
 function App() {
   const [input, setinput] = useState<string>("");
   const todo = useSelector((state: reducerType) => state.todo);
   const dispatch = useDispatch();
 
-
   return (
     <div className="App">
-   <div>
-   <input type="text" value={input} onChange={(e)=>{
-      setinput(e.target.value)
-    }}/> 
-    <button onClick={() => {
-      dispatch(addTodo(input));
-      setinput("");
-    }}>Add Todo</button>
-   </div>
-   <div className="todo">
-  {todo.length !== 0 ? (   <ul>
-     {todo.map((note :string , index: number) => (
-       <li key={index}>
-         <h3>{note}</h3>
-         <button onClick={()=> {
-           dispatch(deleteTodo(index));
-         }}>Delete Todo</button>
-       </li>
-     ))}
-     </ul>) : (<h2>Create New Todo</h2>)}
-   </div>
+      <div>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => {
+            setinput(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            dispatch(addTodo(input));
+            setinput("");
+          }}
+        >
+          Add Todo
+        </button>
+      </div>
+      <div className="todo">
+        {todo.length !== 0 ? (
+          <ul>
+            {todo.map((note: string, index: number) => (
+              <li key={index}>
+                <h3>{note}</h3>
+                <button
+                  onClick={() => {
+                    dispatch(deleteTodo(index));
+                  }}
+                >
+                  Delete Todo
+                </button>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <h2>Create New Todo</h2>
+        )}
+      </div>
     </div>
   );
 }
